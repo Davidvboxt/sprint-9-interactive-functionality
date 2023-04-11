9019e8a341d0b2d8f8b68d1b689d2aa5dd1995f4
Davidvboxt <davidvb00@gmail.com>
2023-04-11 20:50:31 +0200

    update

--- public/index.js
+++ public/index.js
@@ -1,22 +1,3 @@
-// READ MORE 
-const parentContainer =  document.querySelector('.read-more-container');
-
-parentContainer.addEventListener('click', event=>{
-
-    const current = event.target;
-
-    const isReadMoreBtn = current.className.includes('read-more-btn');
-
-    if(!isReadMoreBtn) return;
-
-    const currentText = event.target.parentNode.querySelector('.read-more-text');
-
-    currentText.classList.toggle('read-more-text--show');
-
-    current.textContent = current.textContent.includes('Minder Details') ? "Meer Details" : "Minder Details";
-
-})
-
 // SLIDER
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
